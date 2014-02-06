6e40e30fa8eaa8f0ca05518cf184ac66e954a1b0
Eugenio Carocci <carocci.eugenio@gmail.com>
2014-02-06 12:46:36 +0100

    Modificato main.

--- ihotel/src/Main.java
+++ ihotel/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import com.iHotel.controller.*;
 import com.iHotel.model.*;
 
@@ -7,6 +9,15 @@ public class Main {
 		// TODO Auto-generated method stub
 		CGestisciPrenotazione gestisciPrenotazione = CGestisciPrenotazione.getInstance();
 		gestisciPrenotazione.creaNuovaPrenotazione();
+		// 1 Gennaio 2014 - 00:00.00
+		long dataInizio = 1388530800000L;
+		// 7 Gennaio 2014 - 23:59.59
+		long dataFine = 1389135599000L;
+		// Tipologie
+		ArrayList<String> tipologie = new ArrayList<String>();
+		tipologie.add("Singola");
+		gestisciPrenotazione.cercaCamereLibere(dataInizio, dataFine, tipologie);
+		
 	}
 
 }
