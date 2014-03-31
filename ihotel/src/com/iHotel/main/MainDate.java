019ca2fe02964b25de1fb630255b759659bea56e
Eugenio Carocci <carocci.eugenio@gmail.com>
2014-03-31 21:37:51 +0200

    Aggiunto Main di comparazione tra date.

---
+++ ihotel/src/com/iHotel/main/MainDate.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.iHotel.main;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * @author Eugenio
+ *
+ */
+public class MainDate {
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GregorianCalendar gc1 = new GregorianCalendar();
+		gc1.clear();
+		gc1.set(2014, 0, 11);
+		
+		GregorianCalendar gc2 = new GregorianCalendar();
+		gc2.clear();
+		gc2.set(2014, 0, 10);
+		
+		System.out.println(gc2.compareTo(gc1));
+		System.out.println(gc1.get(Calendar.DATE)+"-"+gc1.get(Calendar.MONTH)+"-"+gc1.get(Calendar.YEAR)+
+							" "+gc1.get(Calendar.HOUR_OF_DAY)+":"+gc1.get(Calendar.MINUTE)+":"+gc1.get(Calendar.SECOND));
+		System.out.println(gc2.get(Calendar.DATE)+"-"+gc2.get(Calendar.MONTH)+"-"+gc2.get(Calendar.YEAR)+
+				" "+gc2.get(Calendar.HOUR_OF_DAY)+":"+gc2.get(Calendar.MINUTE)+":"+gc2.get(Calendar.SECOND));
+	}
+
+}
