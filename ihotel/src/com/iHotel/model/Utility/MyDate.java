fb9107d4337421b64358c8fdffed79b542021ce3
Eugenio Carocci <carocci.eugenio@gmail.com>
2014-04-01 12:46:24 +0200

    MyDate

---
+++ ihotel/src/com/iHotel/model/Utility/MyDate.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.iHotel.model.Utility;
+
+import java.util.GregorianCalendar;
+
+/**
+ * @author Eugenio
+ *
+ */
+@SuppressWarnings("serial")
+public class MyDate extends GregorianCalendar {
+	public MyDate() {
+		super();
+		this.clear();
+	}
+}
