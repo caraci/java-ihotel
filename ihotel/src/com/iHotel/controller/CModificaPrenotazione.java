9996e9921be559bce9ed989dc90f45d4812ef0f3
Eugenio Carocci <carocci.eugenio@gmail.com>
2014-04-04 10:19:40 +0200

    Implementati metodi nel controllore CModificaPrenotazione.

--- ihotel/src/com/iHotel/controller/CModificaPrenotazione.java
+++ ihotel/src/com/iHotel/controller/CModificaPrenotazione.java
@@ -5,12 +5,18 @@ import java.util.ArrayList;
 
 import com.iHotel.model.Albergo.Albergo;
 import com.iHotel.model.Albergo.PrenotazioneSubject;
+import com.iHotel.model.Albergo.ServizioInterno;
 import com.iHotel.model.Albergo.Storico;
+import com.iHotel.model.Albergo.Cataloghi.CatalogoServiziInterni;
+import com.iHotel.model.Albergo.Cataloghi.DescrizioneServizioInterno;
 import com.iHotel.model.ForeignSystem.ServizioEsterno;
 import com.iHotel.model.State.CameraContext;
 import com.iHotel.model.Utility.MyDate;
 import com.iHotel.model.Utility.Periodo;
+import com.iHotel.model.Utility.Prezzo;
+import com.iHotel.view.VFrameHome;
 import com.iHotel.view.Access.ViewHandler;
+import com.iHotel.view.GestionePrenotazione.VFGP_AggiungiServiziInterni;
 import com.iHotel.view.GestionePrenotazione.VFGP_InfoCamera;
 import com.iHotel.view.GestionePrenotazione.VFGP_InfoPrenotazione;
 import com.iHotel.view.GestionePrenotazione.VFGP_RicercaPrenotazioneDaCodice;
@@ -34,7 +40,6 @@ public class CModificaPrenotazione {
 	/* ------------------------------- Metodi di classe --------------------------------------- */
 	/**
 	 * Metodo per ottenere l'instanza di questa classe - Pattern Singleton.
-	 * @return Instanza unica di questa classe
 	 * @throws IOException 
 	 */
     public static CModificaPrenotazione getInstance() throws IOException {
@@ -49,19 +54,25 @@ public class CModificaPrenotazione {
      * Metodo che termina l'aggiunta dei servizi e restituisce la schermata precedente
      */
     public void terminaAggiuntaServiziCamera(){
-    	
+    	// Mostro l'interfaccia relativa alla camera, utilizzando il metodo del controllore.
+    	gestioneCamera(_camera.get_numero());
     }
     /**
      * Metodo che conclude la gestione della camera
      */
     public void terminaGestioneCamera(){
-    	
+    	// Mostro l'interfaccia relativa alla camera, utilizzando il metodo del controllore.
+    	recuperaPrenotazioneDaCodice(_prenotazione.get_codice());
     }
     /**
      * Metodo che conclude la gestione della prenotazione
      */
     public void terminaGestionePrenotazione(){
-    	
+    	// Creo l'interfaccia iniziale.
+    	VFrameHome frameHome=VFrameHome.getInstance();
+    	frameHome.creaFrame();
+    	// Mostro l'interfaccia iniziale.
+    	ViewHandler.getInstance().showFrame(frameHome);
     }
     /**
      * Metodo per aggiungere un servizio alla camera selezionata, relativa alla prenotazione che si sta
@@ -69,41 +80,58 @@ public class CModificaPrenotazione {
      * @param dataServizio Data per il quale si richiede il servizio interno.
      */
     public void aggiungiServizio(MyDate dataServizio){
-    	
+    	// Creo il nuovo oggetto di tipo ServizioInterno
+    	ServizioInterno servizioInterno = new ServizioInterno();
+    	servizioInterno.set_codice(_codiceServizio);
+    	servizioInterno.set_data(dataServizio);
+    	// Ricavo il periodo della prenotazione
+    	Periodo periodo = _prenotazione.get_periodo();
+    	// Aggiungo il servizio interno alla Camera che si sta gestendo, fornendo periodo e servizio.
+    	_camera.aggiungiServizioInPeriodo(servizioInterno, periodo);
     }
     /**
      * Metodo per mostrare l'interfaccia per aggiungere nuovi servizi alla camera della prenotazione.
      */
     public void aggiungiServiziCamera(){
-    	
+    	VFGP_AggiungiServiziInterni aggiungiServiziInterni = VFGP_AggiungiServiziInterni.getInstance();
+    	// Creo l'interfaccia fornendo l'insieme di descrittori dei servizi interni
+    	aggiungiServiziInterni.creaFrame(CatalogoServiziInterni.getInstance().get_descrizioneServizi());
+    	// Mostro vfgpAggiungiServiziInterni
+    	ViewHandler.getInstance().showFrame(aggiungiServiziInterni);
     }
     /**
      * Metodo per mostrare le informazioni relative ad un servizio interno.
      * @param codiceServizio Codice del servizio da mostrare
      */
-    public void scegliServizioDaCodice(String codiceServizio){
+    public DescrizioneServizioInterno scegliServizioDaCodice(String codiceServizio){
+    	// Setto il codice del servizio scelto come attributo del controllore
     	_codiceServizio=codiceServizio;
+    	// Fornisco il descrittore all'interfaccia.
+    	return CatalogoServiziInterni.getInstance().getDescrizioneServizioDaCodice(_codiceServizio);
     }
     /**
      * Metodo per mostrare l'interfaccia 
      */
 	public void gestionePrenotazione() {
-		VFGP_RicercaPrenotazioneDaCodice frameInserimentoCodicePrenotazione = VFGP_RicercaPrenotazioneDaCodice.getInstance();
-		frameInserimentoCodicePrenotazione.creaFrame();
-		ViewHandler.getInstance().showFrame(frameInserimentoCodicePrenotazione);
+		VFGP_RicercaPrenotazioneDaCodice ricercaPrenotazioneDaCodice = VFGP_RicercaPrenotazioneDaCodice.getInstance();
+		ricercaPrenotazioneDaCodice.creaFrame();
+		ViewHandler.getInstance().showFrame(ricercaPrenotazioneDaCodice);
 	}
 	/**
 	 * Metodo per mostrare l'interfaccia per la gestione della prenotazione, relativa al codice fornito.
 	 * @param codicePrenotazione Codice della prenotazione da caricare.
 	 */
 	public void recuperaPrenotazioneDaCodice(String codicePrenotazione) {
-		//System.out.print(codicePrenotazione.toString());
+		// Recupero la prenotazione dallo storico.
 		_prenotazione=Storico.getInstance().recuperaPrenotazioneDaCodice(codicePrenotazione);
+		// Recupero il prezzo dei servizi esterni della prenotazione.
+		Prezzo prezzo=_albergo.getPrezzoServiziEsterniPrenotazione(_prenotazione);
 		
-		VFGP_InfoPrenotazione infoPrenotazione= VFGP_InfoPrenotazione.getInstance();
-		infoPrenotazione.creaFrame(_prenotazione);
+		VFGP_InfoPrenotazione infoPrenotazione=VFGP_InfoPrenotazione.getInstance();
+		// Creo l'interfaccia relativa alla prenotazione
+		infoPrenotazione.creaFrame(_prenotazione,prezzo);
+		// Mostro l'interfaccia relativa alla gestione della prenotazione
 		ViewHandler.getInstance().showFrame(infoPrenotazione);
-		
 	}
 
 	/**
@@ -111,17 +139,17 @@ public class CModificaPrenotazione {
 	 * @param numeroCamera 
 	 */
 	public void gestioneCamera(String numeroCamera) {
-		// Ricavo la camera da visualizzare.
+		// Ricavo la camera da visualizzare e la salvo come attributo del controllore.
 		_camera=_albergo.getCameraDaNumero(numeroCamera);
 		// Ricavo il periodo della prenotazione.
 		Periodo periodo = _prenotazione.get_periodo();
 		// Ricavo i servizi esterni della camera nel periodo della prenotazione.
 		ArrayList<ServizioEsterno> serviziEsterni = _albergo.getElencoServiziEsterniCameraInPeriodo(_camera,periodo);
 		// Preparo l'interfaccia da visualizzare
-		VFGP_InfoCamera vfgpInfoCamera = VFGP_InfoCamera.getInstance();
-		vfgpInfoCamera.creaFrame(_camera, serviziEsterni);
+		VFGP_InfoCamera infoCamera = VFGP_InfoCamera.getInstance();
+		infoCamera.creaFrame(_camera, serviziEsterni);
 		// Visualizzo la nuova interfaccia.
-		ViewHandler.getInstance().showFrame(vfgpInfoCamera);
+		ViewHandler.getInstance().showFrame(infoCamera);
 	}
 	/* ----------------------- Getter, Setter -------------------------- */
 
---
+++ ihotel/src/com/iHotel/view/GestionePrenotazione/VFGP_AggiungiServiziInterni.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.iHotel.view.GestionePrenotazione;
+
+import java.util.HashMap;
+
+import com.iHotel.model.Albergo.Cataloghi.DescrizioneServizioInterno;
+import com.iHotel.view.View;
+
+/**
+ * @author Eugenio
+ *
+ */
+@SuppressWarnings("serial")
+public class VFGP_AggiungiServiziInterni extends View {
+	/* ----------------------------- Attributi e Costruttore ----------------- */
+	// Singleton
+	private static VFGP_AggiungiServiziInterni instance=null;
+	/**
+	 * Costruttore privato - Pattern Singleton
+	 */
+	private VFGP_AggiungiServiziInterni() {}
+	/* ------------------------------- Metodi di classe -------------------------- */
+	/**
+	 * Metodo per ottenere l'unica instanza di questa classe - Pattern singleton
+	 */
+	public static VFGP_AggiungiServiziInterni getInstance() {
+    	if(instance == null) {
+            instance = new VFGP_AggiungiServiziInterni();
+         }
+         return instance;
+    }
+	/* ------------------------------ Metodi di instanza ------------------------ */
+	public void creaFrame(HashMap<String,DescrizioneServizioInterno> descrizioniServizi) {
+		
+	}
+}
