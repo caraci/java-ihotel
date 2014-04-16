1a5041646f63d9db779127c9872554a6a0a603b0
Alessandro Ranalli <ranalli.alessandro1@gmail.com>
2014-04-16 08:58:42 +0200

    implementati metodi con nuovi colori in LightStyleFactory

--- ihotel/src/com/iHotel/view/Access/LightStyleFactory.java
+++ ihotel/src/com/iHotel/view/Access/LightStyleFactory.java
@@ -4,6 +4,7 @@
 package com.iHotel.view.Access;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -20,6 +21,8 @@ import javax.swing.JTextField;
 public class LightStyleFactory implements StyleAbstractFactory {
 
 	/* -------------------------------- Attributi e costruttore -------------------------------*/
+
+	//singleton
 	private static LightStyleFactory instance = null;
 	/**
 	 * Costruttore privato - pattern Singleton
@@ -41,7 +44,7 @@ public class LightStyleFactory implements StyleAbstractFactory {
 	@Override
 	public JPanel getPanel() {
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
+		panel.setBackground(new Color(200, 200, 200));
 		panel.setBorder(BorderFactory.createLineBorder(Color.black));
 		return panel;
 	}
@@ -50,36 +53,55 @@ public class LightStyleFactory implements StyleAbstractFactory {
 	@Override
 	public JLabel getLabel() {
 		JLabel label = new JLabel();
+		label.setBackground(new Color(150, 150, 150));
+		label.setForeground(new Color(20,20,20));
 		return label;
 	}
 	@Override
 	public JLabel getLabelIntestazione_1() {
-		// TODO Auto-generated method stub
-		return null;
+		JLabel label=getLabel();
+		// Setto il font.
+		Font font = new Font("Serif", Font.BOLD, 20);
+		label.setFont(font);
+		return label;
 	}
 	@Override
 	public JLabel getLabelIntestazione_2() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		JLabel label=getLabel();
+		// Setto il font.
+		Font font = new Font("Serif", Font.BOLD, 16);
+		label.setFont(font);
+		return label;	}
 	@Override
 	public JButton getButton() {
 		JButton button = new JButton();
+		button.setBackground(new Color(200, 200, 200));
+		button.setForeground(new Color(30,30,30));
 		return button;
 	}
 	@Override
 	public JButton getButtonAvanti() {
-		// TODO Auto-generated method stub
-		return null;
+		JButton button=getButton();
+		// Setto il font.
+		Font font = new Font("Serif", Font.BOLD, 12);
+		button.setFont(font);
+		// Setto colori - Sfondo Vere - Testo Nero
+		button.setBackground(new Color(127,255,0));
+		button.setForeground(new Color(0,0,0));
+		return button;
 	}
 	@Override
 	public JTextField getTextField() {
 		JTextField text = new JTextField();
+		text.setBackground(new Color(255, 255, 255));
+		text.setForeground(new Color(0, 0, 0));
 		return text;
 	}
 	@Override
 	public JCheckBox getCheckBox() {
 		JCheckBox checkBox = new JCheckBox();
+		checkBox.setBackground(new Color(255, 255, 255));
+		checkBox.setForeground(new Color(0, 0, 0));
 		return checkBox;
 	}
 	
