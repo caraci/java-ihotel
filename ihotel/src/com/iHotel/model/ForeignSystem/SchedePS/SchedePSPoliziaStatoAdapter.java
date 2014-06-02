fe2c57188ef8345c47f51766d44b33c076831807
Alessandro Ranalli <ranalli.alessandro1@gmail.com>
2014-06-02 10:53:18 +0200

    Eliminato ciclo for dal sistema esterno per le schede ps

--- ihotel/src/com/iHotel/model/ForeignSystem/SchedePS/SchedePSPoliziaStatoAdapter.java
+++ ihotel/src/com/iHotel/model/ForeignSystem/SchedePS/SchedePSPoliziaStatoAdapter.java
@@ -3,12 +3,7 @@
  */
 package com.iHotel.model.ForeignSystem.SchedePS;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
-import com.iHotel.model.Albergo.Camera.Camera;
 import com.iHotel.model.Albergo.Soggiorno.SoggiornoContextSubject;
-import com.iHotel.model.Persona.Ospite;
 
 /**
  * Classe concreta adattatore al sistema di generazione di schedine di pubblica sicurezza della polizia di stato.
@@ -19,15 +14,7 @@ public class SchedePSPoliziaStatoAdapter implements ISchedePSAdapter {
 
 	@Override
 	public void generaSchedePubblicaSicurezza(SoggiornoContextSubject soggiorno) {
-		/*Recupero le camere prenotate*/
-		ArrayList<Camera> camerePrenotate = soggiorno.get_camerePrenotate();
-		/*Ciclo sulle camere della prenotazione*/
-		for (Camera camera : camerePrenotate) {
-			/*Per ogni ospite presente nella camera genero la scheda di PS*/
-			for (Iterator<Ospite> iterator = camera.getOspitiInPeriodo(soggiorno.get_periodo()).iterator(); iterator.hasNext();) {
-				//TODO generare schede di pubblica sicurezza
-			}
-		}
+		//TODO implementare generazione schedine di pubblica sicurezza
 	}
 
 }
